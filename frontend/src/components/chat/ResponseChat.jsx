22d080744b6bcbf01bae68dd27828f68b7adf034
lavanyagarg112 <lavanyagarg@u.nus.edu>
2024-06-08 09:55:47 +0800

    response cant be clicked

--- frontend/src/components/chat/ResponseChat.jsx
+++ frontend/src/components/chat/ResponseChat.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import classes from './Chat.module.css'
 
-const Response = ({text, onClick}) => {
+const Response = ({text}) => {
   return (
     <div className={classes.response}>
       {text}
