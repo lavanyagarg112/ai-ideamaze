3462a78193bb71e795fc7dc24566159372c2e6be
lavanyagarg112 <lavanyagarg@u.nus.edu>
2024-06-09 00:05:13 +0800

    user can click on chat box to generate new idea

--- frontend/src/pages/ChatBox.jsx
+++ frontend/src/pages/ChatBox.jsx
@@ -5,7 +5,7 @@ import Response from '../components/chat/ResponseChat'
 
 import classes from './Home.module.css'
 
-const ChatBox = ({messages, addMessage, newBranch, userName, latest}) => {
+const ChatBox = ({messages, addMessage, newBranch, userName, latest, handleClick}) => {
 
   const currentLatest = messages[messages.length - 1]
   const [children, setChildren] = useState(false)
@@ -41,7 +41,7 @@ const ChatBox = ({messages, addMessage, newBranch, userName, latest}) => {
         {console.log("messages: ", messages)}
         {messages.length === 0 ? messages : messages.map((message) => {
           if (message.role === 'user') {
-            return <Query key={message.id} text={message.text} onClick={() => newBranch('0', false, '')} canChange={false}/>
+            return <Query key={message.id} text={message.text} onClick={() => handleClick(message.id)} canChange={false}/>
           } else if (message.role === 'assistant') {
             return <Response key={message.id} text = {message.text} />
           } else {
